4de0d4d4a5f74a957a0a6766a8217c6b5d8d1632
baltej223 <baltej963@gmail.com>
2025-11-08 19:21:41 +0530

    Did final changes

--- locar_vite/src/components/Login.tsx
+++ locar_vite/src/components/Login.tsx
@@ -12,7 +12,7 @@ const Login: React.FC = () => {
     const provider = new GoogleAuthProvider();
 
     try {
-      let user = await signInWithPopup(auth, provider);
+      await signInWithPopup(auth, provider);
       alert("Login successful!");
       
     } catch (err: any) {
