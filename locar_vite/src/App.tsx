57855989b906190d3e3586872c55303a4b6e3219
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-11-08 19:03:52 +0530

    added react router

--- locar_vite/src/App.tsx
+++ locar_vite/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Login from './components/Login'
-// import MainScreen from './components/MainScreen'
+import MainScreen from './components/MainScreen'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    {/* <MainScreen /> */}
-    <Login/>
+      <Routes>
+        <Route path="/" element={<MainScreen />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
