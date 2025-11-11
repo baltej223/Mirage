e2b503716fdf118e1a50be7232c3e3ea21cc9fdb
baltej223 <baltej963@gmail.com>
2025-11-11 23:13:57 +0530

    Added loader component

--- locar_vite/src/components/ProtectedRoute.tsx
+++ locar_vite/src/components/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import type { ReactNode } from "react";
+import Loader from "../components/loader.tsx";
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { user, loading } = useAuth();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <Loader />;
   if (!user) return <Navigate to="/login" />;
 
   return <>{children}</>;
